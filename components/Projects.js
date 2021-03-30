55fe4b402d862b6ff7709f25ef230aaaddc0dad4
unknown <mdaley115@gmail.com>
2021-03-30 12:45:58 -0400

    added project, removed template project cards

--- components/Projects.js
+++ components/Projects.js
@@ -1,17 +1,14 @@
 import ProjectCard from './ProjectCard'
-const description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam accumsan quam non ante suscipit hendrerit accumsan eu purus. Integer tempus efficitur luctus. Praesent arcu arcu, blandit at cursus sit amet, vestibulum at elit. Quisque a nisi et tortor molestie hendrerit nec vitae nulla. Phasellus convallis ligula non maximus dictum. Donec at nulla a nunc congue vestibulum eu non ante."
-const git = ""
-const demo = ""
+const description = "A mobile & tablet responsive single site web page that displays hand drawn and animated art by a close friend of mine. Utilizes react-scroll for a smooth transition back to the top of the page, cloudinary to host the images and next/react to piece it all together."
+const git = "https://github.com/M-Daley/photogallery"
+const demo = "https://lit-forest-23513.herokuapp.com/"
+const title = "Art Gallery"
 
 export default function Projects({ aos }) { 
 return(
     <div id="Projects" className="page-container">
         <div data-aos="fade-up" className="project-container">
-                <ProjectCard position={1} title="Title Soon" git={git} demo={demo} description={description} image="/assets/stock_site.jpg"/>
-                <ProjectCard position={2} title="Title Soon" git={git} demo={demo} description={description} image="/assets/stock_site.jpg"/>
-                <ProjectCard position={3} title="Title Soon" git={git} demo={demo} description={description} image="/assets/stock_site.jpg"/>
-                <ProjectCard position={4} title="Title Soon" git={git} demo={demo} description={description} image="/assets/stock_site.jpg"/>
-                <ProjectCard position={5} title="Title Soon" git={git} demo={demo} description={description} image="/assets/stock_site.jpg"/>
+                <ProjectCard position={1} title={title} git={git} demo={demo} description={description} image="/assets/gallery.png"/>
             </div>
     </div>
 )}
