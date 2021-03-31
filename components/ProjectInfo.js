8e09e65ea2a2559b5199d4d36f083cf7d4ae9723
unknown <mdaley115@gmail.com>
2021-03-31 12:21:10 -0400

    added project

---
+++ components/ProjectInfo.js
@@ -0,0 +1,18 @@
+export default ProjectInfo = {
+    artGallery: {
+        position: 1,
+        description: "A mobile & tablet responsive single site web page that displays hand drawn and animated art by a close friend of mine. Utilizes react-scroll for a smooth transition back to the top of the page, cloudinary to host the images and next/react to piece it all together.",
+        git: "https://github.com/M-Daley/photogallery",
+        demo: "https://lit-forest-23513.herokuapp.com/",
+        image: "/assets/gallery.png",
+        title: "Art Gallery"
+    },
+    responsive: {
+        position: 2,
+        description: "A single page sample responsive page sizable from desktop/tablet to mobile. Utilizes new unique css selectors that add dynamic spacing and position of all elements given a custom pattern ensuring no matter how many paragraphs or images you want to be place wherever in this page it'll be adjusted accordingly with no extra coding or time spent debugging.",
+        git: "https://github.com/M-Daley/responsive_react",
+        demo: "https://responsivereact.herokuapp.com",
+        image: "/assets/responsive.png",
+        title: "Responsive Info Page"
+    }
+}
--- components/Projects.js
+++ components/Projects.js
@@ -1,14 +1,12 @@
 import ProjectCard from './ProjectCard'
-const description = "A mobile & tablet responsive single site web page that displays hand drawn and animated art by a close friend of mine. Utilizes react-scroll for a smooth transition back to the top of the page, cloudinary to host the images and next/react to piece it all together."
-const git = "https://github.com/M-Daley/photogallery"
-const demo = "https://lit-forest-23513.herokuapp.com/"
-const title = "Art Gallery"
+import ProjectInfo from './ProjectInfo'
 
 export default function Projects({ aos }) { 
 return(
     <div id="Projects" className="page-container">
         <div data-aos="fade-up" className="project-container">
-                <ProjectCard position={1} title={title} git={git} demo={demo} description={description} image="/assets/gallery.png"/>
+                <ProjectCard {...ProjectInfo.artGallery}/>
+                <ProjectCard {...ProjectInfo.responsive}/>
             </div>
     </div>
 )}
