b4ba33e331f649f42f36569fe2b93102aee52093
unknown <mdaley115@gmail.com>
2021-04-14 23:20:04 -0400

    added project + removed borders

--- components/ProjectInfo.js
+++ components/ProjectInfo.js
@@ -1,6 +1,14 @@
 const ProjectInfo = {
-    artGallery: {
+    audioPresentation: {
         position: 1,
+        description: "Custom audio track controllers, responsive images and columns on top of smooth scroll and state management without Redux using pure CSS for styling.",
+        git: "https://github.com/M-Daley/presentation",
+        demo: "https://dashboard.heroku.com/apps/evening-meadow-30528/deploy/github",
+        image: "/assets/presentation.png",
+        title: "Audio Presentation"
+    },
+    artGallery: {
+        position: 2,
         description: "A mobile & tablet responsive single site web page that displays hand drawn and animated art by a close friend of mine. Utilizes react-scroll for a smooth transition back to the top of the page, cloudinary to host the images and next/react to piece it all together.",
         git: "https://github.com/M-Daley/photogallery",
         demo: "https://lit-forest-23513.herokuapp.com/",
@@ -8,7 +16,7 @@ const ProjectInfo = {
         title: "Art Gallery"
     },
     responsive: {
-        position: 2,
+        position: 3,
         description: "A single page sample responsive page sizable from desktop/tablet to mobile. Utilizes new unique css selectors that add dynamic spacing and position of all elements given a custom pattern ensuring no matter how many paragraphs or images you want to be place wherever in this page it'll be adjusted accordingly with no extra coding or time spent debugging.",
         git: "https://github.com/M-Daley/responsive_react",
         demo: "https://responsivereact.herokuapp.com",
@@ -16,7 +24,7 @@ const ProjectInfo = {
         title: "Responsive Info Page"
     },
     blog: {
-        position: 3,
+        position: 4,
         description: "Blog built using a markup to html read and Next.js. Used for templating and introductory to Next.js as well as Vercel Web hosting.",
         git: "https://github.com/M-Daley/nextjs-blog",
         demo: "https://nextjs-blog-black-six.vercel.app/",
--- components/Projects.js
+++ components/Projects.js
@@ -1,10 +1,11 @@
 import ProjectCard from './ProjectCard'
 import ProjectInfo from './ProjectInfo'
 
-export default function Projects({ aos }) { 
+export default function Projects() { 
 return(
     <div id="Projects" className="page-container">
         <div data-aos="fade-up" className="project-container">
+                <ProjectCard {...ProjectInfo.audioPresentation}/>
                 <ProjectCard {...ProjectInfo.artGallery}/>
                 <ProjectCard {...ProjectInfo.responsive}/>
                 <ProjectCard {...ProjectInfo.blog}/>
